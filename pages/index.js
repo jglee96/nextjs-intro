dfd14615254bcbeebcb8ccb1018d259da9db68d2
jglee96 <zakklee96@gmail.com>
2022-07-05 22:14:59 +0900

    [feat] Server Side Rendering
    * index.js에서 getServerSideProps 함수를 정의하면 next js는
    getServerSideProps를 서버에서 실행하고, 실행된 데이터를 기반으로
    서버에서 rendering하여 페이지에 보여줌
    * 따라서 data만 바로 가져온다면 로딩 화면 없이 사용자에게 화면을 보여줄
    수 있음
    * 그러나 data 가져오는 속도가 느리다면 로딩화면도 없는 빈 화면으로 보임

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,11 @@ import Seo from "../components/Seo";
 
 const API_KEY = "4dfd9fb7b538e9c006fd0572377194e9";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div>
       <Seo title="Home" />
-      {movies.length === 0 && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -44,4 +36,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
