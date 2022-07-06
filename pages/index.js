708558c05293f6367e5d5246d67e15b96bcda04b
jglee96 <zakklee96@gmail.com>
2022-07-06 22:46:29 +0900

    [feat] click event 구현
    * 사진과 title click 했을 때 movie 정보가 나오는 page 표시되도록 구현
    * Link를 사용하여 title click시 page 이동되도록 함
    * inline 태그는 block 태그를 감싸면 안되기 때문에 사진이 포함된 div
    태그는 분리하여 onClick 이벤트로 page 이동하도록 함
    * onClick 함수에서 router의 push로 page 이동하도록 함

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,47 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 const API_KEY = "4dfd9fb7b538e9c006fd0572377194e9";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div>
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+              key={movie.id}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
